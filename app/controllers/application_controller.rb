bce345b237854647fc5a328148f8983026bfb483
Pamela Frieze <pfrieze@gmail.com>
2018-07-18 17:55:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
-    user = User.find_by(:username => params[:username])
+    user = User.new(:username => params[:username], :password => params[:password])
 		if params[:username] == "" || params[:password] == ""
         redirect "/failure"
     else
