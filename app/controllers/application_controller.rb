4982d17cbf8621e9836654a76fdd5b257733129f
Pamela Frieze <pfrieze@gmail.com>
2018-07-18 17:41:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
     		if user.save
             redirect "/login"
-        else
+        elsif params[:username] == " " || params[:password] == " "
             redirect "/failure"
         end
   end
